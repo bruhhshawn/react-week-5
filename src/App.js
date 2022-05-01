0f4bc3e34da04d5670ad0ea76887076c0aee4621
bruhhshawn <saradf1996@gmail.com>
2022-05-01 14:36:20 -0700

    added github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is created by Bruhhshawn and is{" "}
+        <a href="https://github.com/bruhhshawn/react-week-5" target="_blank">
+          open sourced on github.
+        </a>
+      </footer>
+    </div>
+  );
 }
