3389e6bd284298637428010d33c734417a532c50
bruhhshawn <saradf1996@gmail.com>
2022-05-02 14:21:06 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is created by Bruhhshawn and is{" "}
-        <a href="https://github.com/bruhhshawn/react-week-5" target="_blank">
-          open sourced on github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Bruhhshawn and is{" "}
+          <a href="https://github.com/bruhhshawn/react-week-5">
+            open sourced on github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
